841fd246a26b2df0cd75674cd69c6282673829a0
Peter Rogers <axyaqehsmq@snkmail.com>
2015-03-21 16:21:09 -0500

    Load a custom model of a monster.

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,8 @@ var example = (function() {
     var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
     var light = new THREE.AmbientLight(0xffffff);
     var camera;
-    var box;
+    var testTriangle;
+    var monster;
 
     function initScene() {
         renderer.setSize (window.innerWidth, window.innerHeight );
@@ -14,34 +15,48 @@ var example = (function() {
 
         camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 1, 1000);
 
-        camera.position.z = 5;
+        camera.position.z = 150;
         scene.add(camera);
 
-        var triangleMaterial = new THREE.MeshBasicMaterial({
-            vertexColors: THREE.VertexColors,
-            side: THREE.DoubleSide
+        // var triangleMaterial = new THREE.MeshBasicMaterial({
+        //     vertexColors: THREE.VertexColors,
+        //     side: THREE.DoubleSide
+        // });
+
+        // var triangleGeometry = new THREE.Geometry();
+
+        // triangleGeometry.vertices.push(new THREE.Vector3( 0.0, 1.0, 0.0 ));
+        // triangleGeometry.vertices.push(new THREE.Vector3( -1.0, 1.0, 0.0 ));
+        // triangleGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0 ));
+
+        // triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+
+        // triangleGeometry.faces[0].vertexColors[0] = new THREE.Color(0xFF0000);
+        // triangleGeometry.faces[0].vertexColors[1] = new THREE.Color(0x00FF00);
+        // triangleGeometry.faces[0].vertexColors[2] = new THREE.Color(0X0000FF);
+
+        // var extrusionSettings = {steps: 10, amount: 2, bevelEnabled: false};
+        // prismGeometry = THREE.ExtrudeGeometry( triangleGeometry, extrusionSettings );
+        // testTriangle = new THREE.Mesh(prismGeometry, triangleMaterial);
+
+        // scene.add(testTriangle);
+
+        // render();
+
+        var loader = new THREE.ColladaLoader(); 
+        loader.options.convertUpAxis = true; 
+        loader.load('models/monster.dae', function(collada) { 
+            monster = collada.scene;
+            scene.add(monster);
+            render();
         });
 
-        var triangleGeometry = new THREE.Geometry();
-
-        triangleGeometry.vertices.push(new THREE.Vector3( 0.0, 1.0, 0.0 ));
-        triangleGeometry.vertices.push(new THREE.Vector3( -1.0, 1.0, 0.0 ));
-        triangleGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0 ));
-
-        triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
-
-        triangleGeometry.faces[0].vertexColors[0] = new THREE.Color(0xFF0000);
-        triangleGeometry.faces[0].vertexColors[1] = new THREE.Color(0x00FF00);
-        triangleGeometry.faces[0].vertexColors[2] = new THREE.Color(0X0000FF);
-
-        var testTriangle = new THREE.Mesh(triangleGeometry, triangleMaterial);
-
-        scene.add(testTriangle);
-
-        render();
     }
 
     function render() {
+        if (monster) {
+            monster.rotation.y += 0.02;
+        }
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
