907933d1b8578f80418ccba4844111e5f986a33a
Peter Rogers <axyaqehsmq@snkmail.com>
2015-03-22 16:01:07 -0500

    Putting spotlights and a PhongMaterial on the spinny cube.

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,10 @@ var example = (function() {
     "use strict"; 
     var scene = new THREE.Scene();
     var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
-    var light = new THREE.AmbientLight(0xffffff);
+    var light = new THREE.SpotLight(0xffffff, 1, 150, 0.2);
+    light.position.set(0,0,90);
+    var sidelight = new THREE.SpotLight(0xffffff, 1, 150, 0.2);
+    sidelight.position.set(0,90,0);
     var camera;
     var box;
 
@@ -11,6 +14,7 @@ var example = (function() {
         document.getElementById("wegl-container").appendChild(renderer.domElement);
 
         scene.add(light);
+        scene.add(sidelight);
 
         camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 1, 1000);
 
@@ -19,7 +23,7 @@ var example = (function() {
 
         box = new THREE.Mesh(
             new THREE.BoxGeometry(20,20,20),
-            new THREE.MeshBasicMaterial({color: 0xFF0000})
+            new THREE.MeshPhongMaterial({color: 0x0033FF, ambient: 0x00FF00, specular: 0x444444, shininess: 30})
         );
 
         box.name="box";
@@ -31,6 +35,8 @@ var example = (function() {
 
     function render() {
         box.rotation.y += 0.01;
+        box.rotation.z += 0.01;
+        box.rotation.x += 0.01;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
